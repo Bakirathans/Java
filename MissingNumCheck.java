5f7586d326513f32f89bfcf0cc70323f05a5646a
Bakirathans <sakthysingaram9@gmail.com>
2026-03-15 17:32:00 +0530

    Brunte Force

--- MissingNumCheck.java
+++ MissingNumCheck.java
@@ -1,6 +1,5 @@
 
 import java.util.Scanner;
-import javax.naming.spi.DirStateFactory;
 
 public class MissingNumCheck {
 
@@ -11,26 +10,27 @@ public class MissingNumCheck {
         return true;
     }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = scan.nextInt();
-        }
-        if(!isSequence(array)){
-            System.out.println("1");
-            return;
-        }
-        int result = 0;
-        for (int i = 0; i < n-2; i++) {
-            if(array[i] + 1 != array[i+1]){
-                result = array[i] + 1;
-                break;
+        try (Scanner scan = new Scanner(System.in)) {
+            int n = scan.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = scan.nextInt();
             }
+            if(!isSequence(array)){
+                System.out.println("1");
+                return;
+            }
+            int result = 0;
+            for (int i = 0; i < n-2; i++) {
+                if(array[i] + 1 != array[i+1]){
+                    result = array[i] + 1;
+                    break;
+                }
+            }
+            if(result == 0){
+                result = n;
+            }
+            System.out.println(result);
         }
-        if(result == 0){
-            result = n;
-        }
-        System.out.println(result);
     }
 }
